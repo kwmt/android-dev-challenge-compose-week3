a4da031f041e7f32d0540f8a1ad55489d7293753
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-13 22:21:32 +0900

    textの一部にunderlineを引いた

--- app/src/main/java/com/example/androiddevchallenge/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/login/LoginScreen.kt
@@ -15,37 +15,30 @@
  */
 package com.example.androiddevchallenge.ui.login
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldColors
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TextFieldDefaults.outlinedTextFieldColors
-import androidx.compose.material.TextFieldDefaults.textFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.components.StyledButton
-import com.example.androiddevchallenge.ui.theme.DevChallengeColors
 import com.example.androiddevchallenge.ui.theme.DevChallengeScaffold
 import com.example.androiddevchallenge.ui.theme.DevChallengeTheme
 import com.example.androiddevchallenge.ui.theme.surfaceColorBackground
@@ -107,18 +100,7 @@ fun LoginScreen(onClickLoginButton: () -> Unit) {
                 singleLine = true,
             )
 
-            val a = buildString {
-
-            }
-            Text(
-                "By clicking below, you agree to our Terms of Use and consent to our Privacy Policy.",
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .paddingFrom(FirstBaseline, before = 24.dp),
-                textAlign = TextAlign.Center,
-                style = DevChallengeTheme.typography.body2,
-                color = DevChallengeTheme.colors.textBody2
-            )
+            AgreeText(modifier = Modifier.align(Alignment.CenterHorizontally))
 
             Spacer(modifier = Modifier.height(16.dp))
             StyledButton(
@@ -126,6 +108,45 @@ fun LoginScreen(onClickLoginButton: () -> Unit) {
             )
         }
     }
+}
+
+@Composable
+private fun AgreeText(modifier: Modifier = Modifier) {
+    val a = buildAnnotatedString {
+        val str =
+            "By clicking below, you agree to our Terms of Use and consent to our Privacy Policy."
+        val tos = "Terms of Use"
+        val startTosIndex = str.indexOf(tos)
+        val endTosIndex = startTosIndex + tos.length
+        val pp = "Privacy Policy"
+        val startPpIndex = str.indexOf(pp)
+        val endPpIndex = startPpIndex + pp.length
+
+        append(str)
+
+        addStyle(
+            style = SpanStyle(
+                textDecoration = TextDecoration.Underline
+            ), start = startTosIndex, end = endTosIndex
+        )
+        addStringAnnotation(
+            tag = "TOS",
+            annotation = "https://github.com/",
+            start = startTosIndex,
+            end = endTosIndex,
+        )
+    }
+
+    ClickableText(
+        text = a,
+        modifier = modifier
+            .paddingFrom(FirstBaseline, before = 24.dp),
+//        textAlign = TextAlign.Center,
+        style = DevChallengeTheme.typography.body2,
+//        color = DevChallengeTheme.colors.textBody2,
+        onClick = {
+        }
+    )
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
