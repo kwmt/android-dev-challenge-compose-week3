150f946df43f19cdc42d5b41e0b3ba8b3fed2584
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-13 12:25:42 +0900

    葉っぱを配置

--- app/src/main/java/com/example/androiddevchallenge/ui/welcome/WelcomScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/welcome/WelcomScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,16 +22,24 @@ import com.example.androiddevchallenge.ui.theme.MyTheme
 @Composable
 fun WelcomeScreen() {
     Box(
-        modifier = Modifier.padding(top = 72.dp)
+        modifier = Modifier
             .fillMaxSize()
             .background(DevChallengeTheme.colors.background)
     ) {
         Image(
             painterResource(id = R.drawable.welcome_bg),
             contentDescription = null,
-            contentScale = ContentScale.FillBounds
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.align(Alignment.TopStart)
         )
-
+        Image(
+            painter = painterResource(id = R.drawable.welcome_illos),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .offset(x = 88.dp)
+                .padding(top = 72.dp)
+        )
         Text("hello")
     }
 }
