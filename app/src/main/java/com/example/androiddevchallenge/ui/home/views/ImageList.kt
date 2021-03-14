ef05003643584b790dff6216fee5e7e39a56490a
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-14 21:42:32 +0900

    format

--- app/src/main/java/com/example/androiddevchallenge/ui/home/views/ImageList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/views/ImageList.kt
@@ -16,7 +16,6 @@
 package com.example.androiddevchallenge.ui.home.views
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
@@ -27,7 +26,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -43,7 +41,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -109,8 +106,7 @@ private fun GardenListItem(garden: Garden) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .offset(x = 8.dp)
-                        .paddingFromBaseline(top = 24.dp)
-                    ,
+                        .paddingFromBaseline(top = 24.dp),
                     color = DevChallengeTheme.colors.secondary
                 )
             }
