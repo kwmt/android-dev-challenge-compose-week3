1cc5d381a8c05490acef41b173f402502087fcaa
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-03-17 10:48:52 +0900

    format

--- app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
@@ -15,7 +15,6 @@
  */
 package com.example.androiddevchallenge.ui.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -31,7 +30,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
